1f5ae36e8e76e44ecf70b17058031fd9a9df4c03
ele828 <ele828@gmail.com>
2019-01-01 13:22:51 +0800

    add unit tests for parser

--- packages/cli/__tests__/parser/parse.ts
+++ packages/cli/__tests__/parser/parse.ts
@@ -3,84 +3,145 @@ import { TranslationBlockParser } from '../../src/parser/parse'
 test('parse basic t func', () => {
   const source = `{{ t() }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(' t() ')
 })
 
 test('parse multiple translation block', () => {
   const source = `{{t()}} {{ t( ) }} {{ t() }}{{t()}}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(4)
+  expect(stmts[0].statement).toEqual('t()')
+  expect(stmts[1].statement).toEqual(' t( ) ')
+  expect(stmts[2].statement).toEqual(' t() ')
+  expect(stmts[3].statement).toEqual('t()')
 })
 
 test('parse t with single quote string', () => {
   const source = `{{ t('key', { value: 'val' }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { value: 'val' }) `)
 })
 
 test('parse t with single quote string and escaped single quote', () => {
   const source = `{{ t('key', { value: 'val\\\' escaped' }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { value: 'val\\' escaped' }) `)
 })
 
 test('parse t with double quote string and escaped double quote', () => {
   const source = `{{ t('key', { value: "val\\\" escaped" }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { value: "val\\" escaped" }) `)
 })
 
 test('parse t with backtick string and escaped backtick', () => {
   const source = '{{ t("key", { value: `val\\\` escaped` }) }}'
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(' t("key", { value: `val\\` escaped` }) ')
 })
 
 test('parse t with double quote string', () => {
-  const source = `{{ t('key', { value: 'val' }) }}`
+  const source = `{{ t("key", { value: "val" }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(' t("key", { value: "val" }) ')
 })
 
 test('parse t with backtick string', () => {
   const source = '{{ t(`key`, { value: `val`}) }}'
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(' t(`key`, { value: `val`}) ')
 })
 
 test('parse with double curly brace inside key', () => {
   const source = `{{ t('{{key}}', { p: 'val' }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('{{key}}', { p: 'val' }) `)
 })
 
 test('parse with double curly barce inside value', () => {
   const source = `{{ t('key', { p: '{{val}}' }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { p: '{{val}}' }) `)
 })
 
 test('parse with double curly barce mismachted inside value', () => {
   const source = `{{ t('key', { p: '{{{{{val}}}}}' }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { p: '{{{{{val}}}}}' }) `)
 })
 
 test('parse with js type inside value', () => {
   const source = `{{ t('key', { i: true, j: false, k: [], m: {}, n: undefined, h: null }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { i: true, j: false, k: [], m: {}, n: undefined, h: null }) `)
 })
 
-// FIXME
-test('parse with nested object inside value', () => {
-  const source = `{{ t('key', { val: { m: [], n: { m: { h: 'str' }}}}) }}`
-  const parser = new TranslationBlockParser(source)
-  parser.parse()
-})
+// // FIXME
+// test('parse with nested object inside value', () => {
+//   // nested object may also contains double close braces
+//   const source = `{{ t('key', { val: { m: [], n: { m: { h: 'str' }}}}) }}`
+//   const parser = new TranslationBlockParser(source)
+//   const stmts = parser.parse()
+//   expect(stmts).toHaveLength(1)
+//   expect(stmts[0].statement).toEqual(` t('key', { val: { m: [], n: { m: { h: 'str' }}}}) `)
+// })
 
 test('parse with t function in value field', () => {
   const source = `{{ t('key', { v: t('key2') }) }}`
   const parser = new TranslationBlockParser(source)
-  parser.parse()
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` t('key', { v: t('key2') }) `)
+})
+
+test('bad case: parse with empty block', () => {
+  const source = `{{}}`
+  const parser = new TranslationBlockParser(source)
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(0)
+})
+
+test('bad case: parse with one space block', () => {
+  const source = `{{ }}`
+  const parser = new TranslationBlockParser(source)
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(1)
+  expect(stmts[0].statement).toEqual(` `)
+})
+
+test('bad case: parse with only left brace', () => {
+  const source = `{{`
+  const parser = new TranslationBlockParser(source)
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(0)
+})
+
+test('bad case: parse with only right brace', () => {
+  const source = `}}`
+  const parser = new TranslationBlockParser(source)
+  const stmts = parser.parse()
+  expect(stmts).toHaveLength(0)
 })
--- packages/cli/src/parser/parse.ts
+++ packages/cli/src/parser/parse.ts
@@ -1,46 +1,57 @@
-enum TokenType {
-  LEFT_CURLY_BRACE = '{',
-  RIGHT_CURLY_BRACE = '}',
-  SINGLE_QUOTE = '\'',
-  DOUBLE_QUOTE = '"',
-  BACK_QUOTE = '`',
-  BACK_SLASH= '\\',
+
+export const enum CharCodes {
+  LEFT_CURLY_BRACE = 0x7B,    // {
+  RIGHT_CURLY_BRACE = 0x7D,   // }
+  SINGLE_QUOTE = 0x27,        // '
+  DOUBLE_QUOTE = 0x22,        // "
+  BACK_QUOTE = 0x60,          // `
+  BACK_SLASH= 0x5C,           // \
+}
+
+class TranslationStatement {
+  constructor(
+    public start: number = 0,
+    public end: number = 0,
+    public statement: string = '',
+  ) {}
 }
 
 /**
- * Parser is responsible for parsing translation block in xwml file
+ * TranslationBlockParser is responsible for parsing translation block in xwml file
  */
 export class TranslationBlockParser  {
   public pos: number = 0
   public blockStart: number = -1
+  private statements: TranslationStatement[] = []
+
   constructor(
     public source: string,
   ) { }
 
   parse() {
     while (!this.eof()) this.parseTranslationBlock()
+    return this.statements
   }
 
   parseTranslationBlock() {
-    if (this.match(TokenType.LEFT_CURLY_BRACE) && this.matchNextChar(TokenType.LEFT_CURLY_BRACE)) {
+    if (this.match(CharCodes.LEFT_CURLY_BRACE) && this.matchNextChar(CharCodes.LEFT_CURLY_BRACE)) {
       console.log('\n{{')
       this.advance()
       this.enterTranslationBlock()
-      this.parseTranslationBlock()
-    } else if (this.match(TokenType.SINGLE_QUOTE)) {
+    } else if (this.match(CharCodes.SINGLE_QUOTE)) {
       this.parseSingleQuoteString()
-    } else if (this.match(TokenType.DOUBLE_QUOTE)) {
+    } else if (this.match(CharCodes.DOUBLE_QUOTE)) {
       this.parseDoubleQuoteString()
-    } else if (this.match(TokenType.BACK_QUOTE)) {
+    } else if (this.match(CharCodes.BACK_QUOTE)) {
       this.parseTemplateString()
     } else if (
-      this.match(TokenType.RIGHT_CURLY_BRACE) && this.matchNextChar(TokenType.RIGHT_CURLY_BRACE)
+      this.match(CharCodes.RIGHT_CURLY_BRACE) && this.matchNextChar(CharCodes.RIGHT_CURLY_BRACE)
     ) {
       console.log('\n}}')
-      const block = this.exitTranslationBlock()
+      const { start, end, block } = this.exitTranslationBlock()
       this.advance()
       console.log('block matched', block)
-      // Pass translation block to babylon parser
+      if (end > start && start !== -1) this.statements.push(new TranslationStatement(start, end, block))
     } else {
       this.advance()
     }
@@ -49,24 +60,24 @@ export class TranslationBlockParser  {
   parseSingleQuoteString() {
     this.advance()
     while (!this.eof()) {
-      if (this.match(TokenType.BACK_SLASH) && this.matchNextChar(TokenType.SINGLE_QUOTE)) {
+      if (this.match(CharCodes.BACK_SLASH) && this.matchNextChar(CharCodes.SINGLE_QUOTE)) {
         console.log('matched escaped single quote')
         this.advance()
-      } else if (this.match(TokenType.SINGLE_QUOTE)) {
-        this.nextChar()
+      } else if (this.match(CharCodes.SINGLE_QUOTE)) {
+        this.advance()
         return
       } else {
-        this.nextChar()
+        this.advance()
       }
     }
   }
 
   parseDoubleQuoteString() {
     while (!this.eof()) {
-      if (this.match(TokenType.BACK_SLASH) && this.matchNextChar(TokenType.DOUBLE_QUOTE)) {
+      if (this.match(CharCodes.BACK_SLASH) && this.matchNextChar(CharCodes.DOUBLE_QUOTE)) {
         console.log('matched escaped double quote')
         this.advance()
-      } else if (this.matchNextChar(TokenType.DOUBLE_QUOTE)) {
+      } else if (this.matchNextChar(CharCodes.DOUBLE_QUOTE)) {
         this.advance()
         return
       }
@@ -75,10 +86,10 @@ export class TranslationBlockParser  {
 
   parseTemplateString() {
     while (!this.eof()) {
-      if (this.match(TokenType.BACK_SLASH) && this.matchNextChar(TokenType.BACK_QUOTE)) {
+      if (this.match(CharCodes.BACK_SLASH) && this.matchNextChar(CharCodes.BACK_QUOTE)) {
         console.log('matched escaped backtick')
         this.advance()
-      } else if (this.matchNextChar(TokenType.BACK_QUOTE)) {
+      } else if (this.matchNextChar(CharCodes.BACK_QUOTE)) {
         this.advance()
         return
       }
@@ -86,12 +97,12 @@ export class TranslationBlockParser  {
   }
 
   /**
-   * Object may also contains }} in nested object declaration
-   * thus object declaration should be ignored
+   * Nested object declaration may also contains doubble end braces,
+   * thus object declaration should also be ignored.
    */
   parseObject() {
     while (!this.eof()) {
-      if (this.matchNextChar(TokenType.RIGHT_CURLY_BRACE)) {
+      if (this.matchNextChar(CharCodes.RIGHT_CURLY_BRACE)) {
         this.advance()
         return
       }
@@ -102,26 +113,21 @@ export class TranslationBlockParser  {
     this.blockStart = this.pos
   }
 
-  exitTranslationBlock(): string {
-    const block = this.source.substring(this.blockStart, this.pos - 1)
+  exitTranslationBlock(): { start: number, end: number, block: string } {
+    const start = this.blockStart
+    const end = this.pos - 1
+    const block = this.source.substring(start, end)
     this.blockStart = -1
-    return block
+    return { start, end, block }
   }
 
-  match(type: TokenType) {
-    return this.peekChar() === type
+  match(code: CharCodes) {
+    return this.source.charCodeAt(this.pos) === code
   }
 
-  matchNextChar(type: TokenType) {
-    return this.nextChar() === type
-  }
-
-  peekChar() {
-    return this.source[this.pos]
-  }
-
-  nextChar() {
-    return this.source[++this.pos]
+  // FIXME: remove this unnecessary helper function
+  matchNextChar(code: CharCodes) {
+    return this.source.charCodeAt(++this.pos) === code
   }
 
   advance() {
